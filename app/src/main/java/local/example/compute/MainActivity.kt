9717fecc0f18a661c286567ce4211b0d05e537b4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-26 15:41:38 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/local/example/compute/MainActivity.kt
@@ -0,0 +1,95 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.compute
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private var compute: Compute? = null
+    private var operandOne: EditText? = null
+    private var operandTwo: EditText? = null
+    private var resultView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        compute = Compute()
+        resultView = findViewById(R.id.operation_result_text_view)
+        // TODO
+    }
+
+    fun onAdd(view: View) {
+        getResult(Compute.Operator.ADD)
+    }
+
+    fun onSub(view: View) {
+        getResult(Compute.Operator.SUB)
+    }
+
+    fun onDiv(view: View) {
+        getResult(Compute.Operator.DIV)
+    }
+
+    fun onMul(view: View) {
+        getResult(Compute.Operator.MUL)
+    }
+
+    private fun getResult(operator: Compute.Operator) {
+        var opOne = 0.0
+        var opTwo = 0.0
+        try {
+            opOne = getOperand(operandOne)
+            opTwo = getOperand(operandTwo)
+        } catch (nfe: NumberFormatException) {
+            Log.e(TAG, "NumberFormatException", nfe)
+            resultView!!.text = getString(R.string.computation_error)
+        }
+
+        var result = ""
+        when (operator) {
+            Compute.Operator.ADD -> result = compute!!.add(opOne, opTwo).toString()
+            Compute.Operator.SUB -> result = compute!!.sub(opOne, opTwo).toString()
+            Compute.Operator.DIV -> result = compute!!.div(opOne, opTwo).toString()
+            Compute.Operator.MUL -> result = compute!!.mul(opOne, opTwo).toString()
+            else -> {
+            }
+        }
+        resultView!!.text = result
+    }
+
+    companion object {
+
+        private val TAG = "CalculatorActivity"
+
+        private fun getOperand(operandEditText: EditText?): Double {
+            val operandText = getOperandText(operandEditText!!)
+            return java.lang.Double.valueOf(operandText)
+        }
+
+        private fun getOperandText(operandEditText: EditText): String {
+            return operandEditText.text.toString()
+        }
+    }
+}
